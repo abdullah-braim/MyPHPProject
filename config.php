2557177fc3bd899d91ecdb711f7ecc6b1f450767
abdullah-braim <81009011+abdullah-braim@users.noreply.github.com>
2025-01-10 11:03:14 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$db_host = "alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$db_user = "hmi1x6hht4i7jsju";
+$db_pass = "t2z76vifho6ysc65";
+$db_name = "at0vk9dy5myd27jd";
+
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
